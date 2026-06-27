74c678e38bdda238123a8f5ea9448234dd4ec3ed
Abdulloh <abdullohhudajnazarov88@gmail.com>
2026-06-27 15:36:37 +0500

    slesh add

--- config/urls.py
+++ config/urls.py
@@ -38,6 +38,6 @@ urlpatterns = [
     path('api/v1/', include('school.urls')),
 
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
