55f64b44ef1f773d1955a655ff053440e0faf59d
abdulloh-21 <malikaxudaynazarova066@gmail.com>
2026-07-01 19:50:24 +0500

    static qosh

--- config/settings.py
+++ config/settings.py
@@ -128,8 +128,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-SWAGGER_SETTINGS = {
-    'USE_SESSION_AUTH': False,
-    'SECURITY_DEFINITIONS': {},
-}
