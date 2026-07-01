ce3d4cd4e8b8cd10a1ff9cb59542e49a0074ae75
abdulloh-21 <malikaxudaynazarova066@gmail.com>
2026-07-01 20:16:42 +0500

    debug false

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-defoult')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
